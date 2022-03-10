11553daa8acd530e8061b40c924e0b00c6e5d515
mauricio <mauricioguimaraes@outlook.com>
2022-03-10 14:20:28 -0300

    first commit

---
+++ src/components/Comment.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="container">
+    <h1>Comentários</h1>
+    <hr>
+    <div class="col-md-12">
+      <form action="" method="post">
+        <FormTodo v-on:add-comment="addComment"></FormTodo>
+      </form>
+    </div>
+    <div class="list-group">
+      <p v-if="comments.length <= 0">Ainda não há comentários</p>
+      <small class="list-group-item" v-for="comment in comments" :key="comment">
+        <h4 class="list-group-item-heading">{{ comment.name }}</h4>
+        <p class="list-group-item-text">{{ comment.message }}</p>
+        <div>
+          <button type="submit" href="#" class="btn btn-danger" v-on:click="deleteComment">Excluir</button>
+        </div>
+      </small>
+    </div>
+    <hr>
+  </div>
+</template>
+
+<script>
+  import FormTodo from "./FormTodo.vue";
+
+  export default {
+    name: 'Comment-list',
+    components: {
+      FormTodo
+    },
+    data() {
+      return {
+        comments: []
+      }
+    },
+    methods: {
+      addComment(comment) {
+        this.comments.push(comment);
+      },
+      deleteComment(index) {
+        this.comments.splice(index, 1);
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/FormTodo.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="form-todo form-group">
+    <label for="name">Nome</label>
+    <input type="text" name="name" class="form-control" id="name" placeholder="Nome" v-model="name">
+  </div>
+  <div class="form-group">
+    <label for="comment">Comentário</label>
+    <textarea name="comment" class="form-control" id="comment" rows="3" v-model="message"></textarea>
+  </div>
+  <button type="button" class="btn btn-primary" style="margin: 10px" v-on:click="addComment">Enviar</button>
+</template>
+
+<script>
+  export default {
+    name: "FormTodo",
+    data: function() {
+      return {
+        name: "",
+        message: ""
+      };
+    },
+
+    methods: {
+      addComment: function () {
+        this.$emit("add-comment", {
+          name: this.name,
+          message: this.message
+        });
+        this.name = "";
+        this.message = "";
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
